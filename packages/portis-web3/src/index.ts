9ae418df7de17442c8cb371cd972b3838d942653
Itay Radotzki <radotzki@gmail.com>
2019-04-04 16:57:08 +0300

    update web3-provider-engine

--- packages/portis-web3/src/index.ts
+++ packages/portis-web3/src/index.ts
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import ProviderEngine from '@portis/web3-provider-engine';
 import CacheSubprovider from '@portis/web3-provider-engine/subproviders/cache.js';
 import FixtureSubprovider from '@portis/web3-provider-engine/subproviders/fixture.js';
