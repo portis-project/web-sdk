0141b710bdf95c2ca7095c3944db99f264e24f99
Tom Keidar <tom.keidar@portis.io>
2019-05-27 19:06:17 +0300

    add eos mainnet network

--- packages/portis-eos/src/networks.ts
+++ packages/portis-eos/src/networks.ts
@@ -29,6 +29,11 @@ export function networkAdapter(network: string | INetwork, gasRelay?: boolean) {
 
 const networks: { [key: string]: INetwork } = {
   eos: {
+    nodeUrl: 'api.eosdetroit.io',
+    nodeProtocol: 'rpc',
+    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+  },
+  eosJungle: {
     nodeUrl: 'https://jungle2.cryptolions.io',
     nodeProtocol: 'rpc',
     chainId: 'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473',
