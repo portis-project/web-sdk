8d3e5fa8c42d8ffd0a704cea60a0937bff661883
katien <katien.nol@shapeshift.io>
2021-03-03 12:51:26 -0700

    Create basic starkware provider with stubs for our expected API, add starkex ABI encoder to project

--- packages/portis-web3/src/starkware/starkwareProvider.ts
+++ packages/portis-web3/src/starkware/starkwareProvider.ts
@@ -1,18 +1,27 @@
 import * as sc from '@authereum/starkware-crypto';
+import { OrderParams, TransferErc20Params, TransferErc721Params, TransferEthParams } from './starkwareTypes';
 
 export default class StarkwareProvider {
-  starkExContractAddress: string;
-
-  getPublicKey(ethAddress: string) {
+  /**
+   * Demo code - logic will be moved into the widget in a later ticket
+   * Do we want to allow users to supply application/index/ethaddress?
+   * */
+  getStarkKey() {
     const mnemonic = 'curve become rib fuel garment engine great spring aisle mandate also host';
     const layer = 'starkex';
     const application = 'starkexdvf';
-    const index = '4';
-    // ethAddress is an address derived from the supplied mnemonic
+    const index = '1';
+    const ethAddress = '0xc536a7ac035015a017146a97f3ef44851eaaef41';
     const path = sc.getAccountPath(layer, application, ethAddress, index);
     const keyPair = sc.getKeyPairFromPath(mnemonic, path);
     return sc.getStarkKey(keyPair);
   }
 
-  signStarkTransaction(tx: string) {}
+  createLimitOrder(params: OrderParams) {}
+
+  transferEth(params: TransferEthParams) {}
+
+  transferErc20(params: TransferErc20Params) {}
+
+  transferErc721(params: TransferErc721Params) {}
 }
