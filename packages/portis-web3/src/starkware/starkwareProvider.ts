461955de0f3094c16dedea76a190447374296641
katien <katien.nol@shapeshift.io>
2021-03-10 14:38:38 -0700

    make registerUser abiEncoder call, this commit also contains transfer logic to be used in a later task

--- packages/portis-web3/src/starkware/starkwareProvider.ts
+++ packages/portis-web3/src/starkware/starkwareProvider.ts
@@ -1,9 +1,31 @@
 import * as sc from '@authereum/starkware-crypto';
-import { OrderParams, TransferErc20Params, TransferErc721Params, TransferEthParams } from './starkwareTypes';
+import StarkwareAbiEncoder from '@authereum/starkware-abi-encoder';
+import {
+  OrderParams,
+  TransferErc20Params,
+  TransferErc721Params,
+  TransferEthParams,
+  TransferParams,
+} from './starkwareTypes';
+import { getAssetId, quantizeAmount, sign, Signature } from '@authereum/starkware-crypto';
 
 export default class StarkwareProvider {
+  private _abiEncoder: StarkwareAbiEncoder;
+
+  constructor() {
+    this._abiEncoder = new StarkwareAbiEncoder();
+  }
+
+  register(ethereumAddress, starkKey, operatorSignature) {
+    const txData = this._abiEncoder.registerUser({
+      ethKey: ethereumAddress,
+      starkKey,
+      operatorSignature,
+    });
+    console.log(txData);
+    return txData;
+  }
   /**
-   * Demo code - logic will be moved into the widget in a later ticket
    * Do we want to allow users to supply application/index/ethaddress?
    * */
   getStarkKey() {
@@ -19,9 +41,65 @@ export default class StarkwareProvider {
 
   createLimitOrder(params: OrderParams) {}
 
-  transferEth(params: TransferEthParams) {}
+  transferEth(params: TransferEthParams): Promise<Signature> {
+    const starkKey = this.getStarkKey();
+    const { vaultId, to, quantum, amount, nonce, expirationTimestamp, condition } = params;
+    return this.transfer({
+      from: {
+        starkKey,
+        vaultId,
+      },
+      to,
+      asset: {
+        type: 'ETH',
+        data: {
+          quantum,
+        },
+      },
+      amount,
+      nonce,
+      expirationTimestamp,
+      condition,
+    });
+  }
 
   transferErc20(params: TransferErc20Params) {}
 
   transferErc721(params: TransferErc721Params) {}
+
+  private getKeypair() {
+    const mnemonic = 'curve become rib fuel garment engine great spring aisle mandate also host';
+    const layer = 'starkex';
+    const application = 'starkexdvf';
+    const index = '1';
+    const ethAddress = '0xc536a7ac035015a017146a97f3ef44851eaaef41';
+    const path = sc.getAccountPath(layer, application, ethAddress, index);
+    const keyPair = sc.getKeyPairFromPath(mnemonic, path);
+    return keyPair;
+  }
+
+  private async transfer(input: TransferParams): Promise<Signature> {
+    const { from, to, asset, amount, nonce, expirationTimestamp, condition } = input;
+    const assetId = getAssetId(asset);
+    const quantizedAmount = quantizeAmount(amount as string, asset.data.quantum as string);
+    const senderVaultId = from.vaultId;
+    const targetVaultId = to.vaultId;
+    const targetKey = input.to.starkKey;
+
+    const msgHash = await this._abiEncoder.transfer({
+      quantizedAmount,
+      nonce,
+      senderVaultId,
+      assetId,
+      targetVaultId,
+      targetKey,
+      expirationTimestamp,
+      condition,
+    });
+
+    console.log(`executing transfer with hash ${msgHash}`);
+    const keypair = this.getKeypair();
+    const starkSignature = await sign(keypair, msgHash);
+    return starkSignature;
+  }
 }
